f193f82ce54fa13058f5af58778c2caad7df2008
carot <some-email@domain.com>
2018-04-07 00:34:46 -0500

    Initital commit: theme registration

---
+++ frontend/CatTran/my-first-theme/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cattran
+ * Date: 4/6/18
+ * Time: 11:47 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::THEME,
+        'frontend/CatTran/my-first-them',
+        __DIR_);
